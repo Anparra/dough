8dbb582ac4487c4bf097dc57a924f4ae20211ec5
Anparra <47276601+Anparra@users.noreply.github.com>
2020-07-10 15:11:56 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,10 @@
 namespace SpriteKind {
     export const fridge = SpriteKind.create()
+    export const refri = SpriteKind.create()
 }
-let fridge1: Sprite = null
-function placeFridge () {
-    fridge1 = sprites.create(img`
+let refri1: Sprite = null
+function Fridge () {
+    refri1 = sprites.create(img`
 . d d d d d d d d d d d d d d d 
 d d 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
 d 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
@@ -20,6 +21,6 @@ d 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1
 d 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
 d 1 b b b b b b b b b b b b b b 
 d b b b b b b b b b b b b b b b 
-`, SpriteKind.fridge)
-    tiles.placeOnTile(fridge1, tiles.getTileLocation(19, 0))
+`, SpriteKind.refri)
+    tiles.placeOnTile(refri1, tiles.getTileLocation(19, 0))
 }
