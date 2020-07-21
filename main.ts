634f4d5d5a346011a297e5b13e978384bfe7cea2
Anparra <47276601+Anparra@users.noreply.github.com>
2020-07-20 20:33:34 -0400

    Update main.ts

--- main.ts
+++ main.ts
@@ -20,7 +20,7 @@ function placeDoughZone4() {
         f c c d c c c c f e e e e e e f
         f c c d c c c c c f e e e e f c
     `, SpriteKind.dough)
-    tiles.placeOnTile(dough1, tiles.getTileLocation(19, 4))
+    tiles.placeOnTile(dough1, tiles.getTileLocation(19, 5))
     dough2 = sprites.create(img`
         f f f f f f f f f f f f f f . .
         c c c c c c c c c c c c c f b .
@@ -39,7 +39,7 @@ function placeDoughZone4() {
         1 d d 1 1 1 c c c c c c d c c f
         c 1 d 1 1 c c c c c c c d c c f
     `, SpriteKind.dough)
-    tiles.placeOnTile(dough2, tiles.getTileLocation(20, 4))
+    tiles.placeOnTile(dough2, tiles.getTileLocation(20, 5))
     dough3 = sprites.create(img`
         f c c d c c c c c c f e e f c c
         f c c d c c c c c c f e e f c c
@@ -58,7 +58,7 @@ function placeDoughZone4() {
         . . f c c c c c f . . . . . . .
         . . . f f f f f . . . . . . . .
     `, SpriteKind.dough)
-    tiles.placeOnTile(dough3, tiles.getTileLocation(19, 5))
+    tiles.placeOnTile(dough3, tiles.getTileLocation(19, 6))
     dough4 = sprites.create(img`
         c c c c c c c c c c c c d c c f
         c c c c c c c c c c c c d c c f
@@ -77,7 +77,7 @@ function placeDoughZone4() {
         . . . . . . . . f c c c c f . .
         . . . . . . . . . f f f f . . .
     `, SpriteKind.dough)
-    tiles.placeOnTile(dough4, tiles.getTileLocation(20, 5))
+    tiles.placeOnTile(dough4, tiles.getTileLocation(20, 6))
 }
 function placeDoughZone5() {
     dough1 = sprites.create(img`
@@ -98,7 +98,7 @@ function placeDoughZone5() {
         f c c d c c c c f e e e e e e f
         f c c d c c c c c f e e e e f c
     `, SpriteKind.dough)
-    tiles.placeOnTile(dough1, tiles.getTileLocation(19, 7))
+    tiles.placeOnTile(dough1, tiles.getTileLocation(19, 8))
     dough2 = sprites.create(img`
         f f f f f f f f f f f f f f . .
         c c c c c c c c c c c c c f b .
@@ -117,7 +117,7 @@ function placeDoughZone5() {
         1 d d 1 1 1 c c c c c c d c c f
         c 1 d 1 1 c c c c c c c d c c f
     `, SpriteKind.dough)
-    tiles.placeOnTile(dough2, tiles.getTileLocation(20, 7))
+    tiles.placeOnTile(dough2, tiles.getTileLocation(20, 8))
     dough3 = sprites.create(img`
         f c c d c c c c c c f e e f c c
         f c c d c c c c c c f e e f c c
@@ -136,7 +136,7 @@ function placeDoughZone5() {
         . . f c c c c c f . . . . . . .
         . . . f f f f f . . . . . . . .
     `, SpriteKind.dough)
-    tiles.placeOnTile(dough3, tiles.getTileLocation(19, 8))
+    tiles.placeOnTile(dough3, tiles.getTileLocation(19, 9))
     dough4 = sprites.create(img`
         c c c c c c c c c c c c d c c f
         c c c c c c c c c c c c d c c f
@@ -155,7 +155,7 @@ function placeDoughZone5() {
         . . . . . . . . f c c c c f . .
         . . . . . . . . . f f f f . . .
     `, SpriteKind.dough)
-    tiles.placeOnTile(dough4, tiles.getTileLocation(20, 8))
+    tiles.placeOnTile(dough4, tiles.getTileLocation(20, 9))
 }
 function placeDoughZone3 () {
     dough1 = sprites.create(img`
